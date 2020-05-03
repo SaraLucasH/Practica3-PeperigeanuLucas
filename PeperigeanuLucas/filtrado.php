04ee85451e73ef30f42f91a7ad2dbf012c4cf67a
SaraLucasH <47327009+SaraLucasH@users.noreply.github.com>
2020-05-03 19:06:25 +0200

    SLH

--- PeperigeanuLucas/filtrado.php
+++ PeperigeanuLucas/filtrado.php
@@ -4,6 +4,70 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>		
+.nombreHospital{
+	font-weight: bold;
+	text-align: "center";
+	font-size: 20px;
+	padding: 10px;
+}
+.grid-container {
+  display: grid;
+  grid-template-areas: 
+    'header header header header header header header header' 
+    'left left middle middle middle1 middle1 right right' 
+    'footer footer footer footer footer footer footer footer';
+  grid-column-gap: 10px;
+  padding-left: 10px;
+  
+}
+
+/* Style the left column */
+.left {
+  grid-area: left;
+  padding: 10px;
+  width:100%;
+  height: auto;
+}
+
+/* Style the middle column */
+.middle1 {
+  grid-area: middle1;
+  padding: 10px;
+  width:100%;
+  height: auto;
+}
+
+/* Style the middle column */
+.middle {
+  grid-area: middle;
+  padding: 10px;
+  width:100%;
+  height: auto;
+}
+
+/* Style the right column */
+.right {
+  grid-area: right;
+  padding: 10px;
+  width:100%;
+  height:auto;
+}
+
+@media (max-width: 300px),(max-height: 255px){
+  .grid-container  {
+    grid-template-areas: 
+      'header header header header header header' 
+      'left left left left left left' 
+      'middle middle middle middle middle middle'
+	  'middle1 middle1 middle1 middle1 middle1 middle1' 	  
+      'right right right right right right' 
+      'footer footer footer footer footer footer';
+  }
+}
+
+</style>
+
  <meta charset="utf-8">
  <title>Filtrado Colaborativo</title>
  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
@@ -18,7 +82,7 @@
 
       	function drawTable() {
         	var dataHospital1 = new google.visualization.arrayToDataTable([
-			['Id del paciente','Compuesto nº','Compuesto nº','Compuesto nº'],
+			['Id del paciente','C1 nº','C2 nº','C3 nº'],
     			<?php 
 				$query = "SELECT paciente_id,r1,r2,r3 FROM (paciente INNER JOIN hecho ON paciente.id = hecho.cliente_id) INNER JOIN filtrado_colaborativo ON paciente.id = filtrado_colaborativo.paciente_id WHERE paciente.id = filtrado_colaborativo.paciente_id AND  hecho.hospital_id=1";
     				$exec = mysqli_query($con,$query);
@@ -43,7 +107,7 @@
     			?> 
     		 ]);
 			 var dataHospital2 = new google.visualization.arrayToDataTable([
-			['Id del paciente','Compuesto nº','Compuesto nº','Compuesto nº'],
+			['Id del paciente','C1 nº','C2 nº','C3 nº'],
     			<?php 
 				$query = "SELECT paciente_id,r1,r2,r3 FROM (paciente INNER JOIN hecho ON paciente.id = hecho.cliente_id) INNER JOIN filtrado_colaborativo ON paciente.id = filtrado_colaborativo.paciente_id WHERE paciente.id = filtrado_colaborativo.paciente_id AND  hecho.hospital_id=2";
     				$exec = mysqli_query($con,$query);
@@ -68,7 +132,7 @@
     			?> 
     		 ]);
 			 var dataHospital3 = new google.visualization.arrayToDataTable([
-			['Id del paciente','Compuesto nº','Compuesto nº','Compuesto nº'],
+			['Id del paciente','C1 nº','C2 nº','C3 nº'],
     			<?php 
 				$query = "SELECT paciente_id,r1,r2,r3 FROM (paciente INNER JOIN hecho ON paciente.id = hecho.cliente_id) INNER JOIN filtrado_colaborativo ON paciente.id = filtrado_colaborativo.paciente_id WHERE paciente.id = filtrado_colaborativo.paciente_id AND  hecho.hospital_id=3";
     				$exec = mysqli_query($con,$query);
@@ -93,7 +157,7 @@
     			?> 
     		 ]);
 			 var dataHospital4 = new google.visualization.arrayToDataTable([
-			['Id del paciente','Compuesto nº','Compuesto nº','Compuesto nº'],
+			['Id del paciente','C1 nº','C2 nº','C3 nº'],
     			<?php 
 				$query = "SELECT paciente_id,r1,r2,r3 FROM (paciente INNER JOIN hecho ON paciente.id = hecho.cliente_id) INNER JOIN filtrado_colaborativo ON paciente.id = filtrado_colaborativo.paciente_id WHERE paciente.id = filtrado_colaborativo.paciente_id AND  hecho.hospital_id=4";
     				$exec = mysqli_query($con,$query);
@@ -130,32 +194,29 @@
     </script>
   	
 </head>
-<body style="background-color: #d5f4e6;">
-	<style>		
-		.grid-container {
-			display: grid;
-			grid-column-gap: 5px;
-			grid-template-columns: none none none none;
-			background-color: #d5f4e6;
-			padding: 10px;
-		}
-
-		.grid-item {
-			background-color: rgba(255, 255, 255, 0.8);
-			border: 1px solid rgba(0, 0, 0, 0.8);
-			padding: 5px;
-			font-size: 15px;
-			text-align: center;
-		}
-	</style>
-	<h3>Recomendaciones de compuesto por hospital</h3>
-	<table>
-        <tr>
-            <td><div id="table_hospital1"></div></td>
-            <td><div id="table_hospital2"></div></td>
-			<td><div id="table_hospital3"></div></td>
-			<td><div id="table_hospital4"></div></td>
-		</tr>	
-    </table>
+<body>
+<div class="header">
+		<h2>Recomendaciones de compuesto por hospital.</h2>
+		<p>	Identificador del paciente | Números  de los compuestos recomendados.</p>
+		<p>	En caso de que no se encuentre ninguna recomendacion, el numero del compuesto vendra dado por una barra ("-").</p>
+	</div>
+<div class="grid-container">
+	<div class="middle">
+		<span class="nombreHospital">Hospital 2</span>
+		<div class="middle"  id="table_hospital2"></div>
+	</div>
+	<div class="middle1">
+		<span class="nombreHospital">Hospital 3</span>
+		<div class="middle1" id="table_hospital3"></div>
+	</div>	
+	<div class="right">
+		<span class="nombreHospital">Hospital 4</span>
+		<div class="right" id="table_hospital4"></div>
+	</div>
+	<div class="left">
+		<span class="nombreHospital">Hospital 1</span>
+		<div class="left" id="table_hospital1"></div>
+	</div>
+</div>
 </body>
 </html>
